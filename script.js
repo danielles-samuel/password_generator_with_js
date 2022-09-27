cd76b1faabd0c6ff27374fdbc343db6bc2adaa33
danielles-samuel <chisomannastesia0@gmail.com>
2022-09-26 18:44:58 -0700

    commit updated

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
    let password=document.getElementById("password");
-
+  //=================FUNCTION THAT GENERATES PASSWORD========'// 
  function genPassword() {
     let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let passwordLength = 12;
+    let passwordLength = 10;
     let password = "";
  for (let i = 0; i <= passwordLength; i++) {
      let charsArray = chars.split('');
 let randPass = Math.floor(Math.random() * charsArray.length);
-    password += charsArray[randPass]
+    password += charsArray[randPass].slice();
   }
         document.getElementById("password").value = password;
  }
 
+
+ // =============== COPY PASSWORD FUNCTION============//
 function copyPassword() {
   let copyText = document.getElementById("password");
   copyText.select();
   document.execCommand("copy");  
 }
+
+//===============CLEAR FUNCTION FOR THE INPUT VALUE
+
+const btn = document.querySelector(".btn3");
+
+btn.addEventListener('click', function clearPassword(){
+  password = "";
+  document.getElementById("password").value = password;
+
+})
  
